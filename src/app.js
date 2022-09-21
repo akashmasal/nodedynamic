8be0a1463782566355f5a7cf657c6a2a153ce2b3
akash <akashmasal64@gmail.com>
2022-09-21 18:29:21 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,53 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 1000;
+const path = require("path");
+const port = process.env.PORT || 1000;
 const conn = require("../src/db/conn");
-app.get("", (req, res) => {
-    res.send("ok");
+const hbs = require("hbs");
+const viewspath = path.join(__dirname,"../templates/views");
+const partialspath = path.join(__dirname,"../templates/partials");
+const staticpath = path.join(__dirname,"../public");
+app.use('/css', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
+app.use('/jquery', express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+app.use(express.static(staticpath));
+app.set("view engine","hbs");
+app.set("views",viewspath);
+hbs.registerPartials(partialspath);
+
+
+
+app.get("",(req,res)=>{
+    res.render("index");
 });
 
-app.listen(port, () => {
-    console.log(`server running at port ${port}`);
+app.get("/about",(req,res)=>{
+    res.render("about");
+});
+
+app.get("/service",(req,res)=>{
+    res.render("service");
+});
+
+app.get("/gallery",(req,res)=>{
+    res.render("gallery");
+});
+
+
+app.get("/contact",(req,res)=>{
+    // res.send("ok");
+    res.render("contact");
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`server running on ${port}`);
 });
