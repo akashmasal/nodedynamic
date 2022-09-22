f7b994666fceb4f80116cd39a30e6f9ea0ca9b18
akash <akashmasal64@gmail.com>
2022-09-22 10:44:37 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -4,46 +4,55 @@ const path = require("path");
 const port = process.env.PORT || 1000;
 const conn = require("../src/db/conn");
 const hbs = require("hbs");
-const viewspath = path.join(__dirname,"../templates/views");
-const partialspath = path.join(__dirname,"../templates/partials");
-const staticpath = path.join(__dirname,"../public");
-app.use('/css', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/css")));
-app.use('/js', express.static(path.join(__dirname,"../node_modules/bootstrap/dist/js")));
-app.use('/jquery', express.static(path.join(__dirname,"../node_modules/jquery/dist")));
+const viewspath = path.join(__dirname, "../templates/views");
+const partialspath = path.join(__dirname, "../templates/partials");
+const staticpath = path.join(__dirname, "../public");
+const Contact = require("../src/models/contact");
+app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
+app.use('/jquery', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
 app.use(express.static(staticpath));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.set("view engine","hbs");
-app.set("views",viewspath);
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "hbs");
+app.set("views", viewspath);
 hbs.registerPartials(partialspath);
 
 
 
-app.get("",(req,res)=>{
+app.get("", (req, res) => {
     res.render("index");
 });
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
-app.get("/service",(req,res)=>{
+app.get("/service", (req, res) => {
     res.render("service");
 });
 
-app.get("/gallery",(req,res)=>{
+app.get("/gallery", (req, res) => {
     res.render("gallery");
 });
 
 
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     // res.send("ok");
     res.render("contact");
 });
 
-app.post("/contact",(req,res)=>{
-    console.log(req.body);
-})
+app.post("/contact", async (req, res) => {
+    try {
+        const contactData = new Contact(req.body);
+        // console.log(contactData);
+        await contactData.save();
+        res.status(200).render("index");
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 
 
@@ -54,6 +63,6 @@ app.post("/contact",(req,res)=>{
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running on ${port}`);
 });
--- src/models/contact.js
+++ src/models/contact.js
@@ -6,9 +6,17 @@ const contactSchema = new mongoose.Schema({
     name : String,
     email : String,
     phone : Number,
-    message : String
+    message : String,
+    date : {
+        type : Date,
+        default:Date.now
+    }
 });
 
 const Contact = new mongoose.model("Contact",contactSchema);
+
+module.exports = Contact;
+
+
 
  
